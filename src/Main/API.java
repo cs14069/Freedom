2d4098249de0e3f5fa7df1912a97bf5b164bc7b7
eom-moe <poyoit@gmail.com>
2017-10-18 13:36:01 +0900

    -1000 <-> 3000

---
+++ src/Main/API.java
@@ -0,0 +1,6 @@
+package Main;
+
+public class API {
+	public static final String APIKEY = "4YCsZcwcQ4SW5Gxgkc5qzK";
+	public static final String APISECRET = "eRdpB56HLIy5mScxFPag0jjh2se4fbv+c57UgSo6EQs=";
+}
